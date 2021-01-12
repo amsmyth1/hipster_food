eb814cc36d5e87cfb2bf74243a9e4c0e8db024a9
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-12 15:14:58 -0500

    Create all tests for foodtruck

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'mocha/minitest'
 require './lib/food_truck'
+require './lib/item'
 
 class FoodTruckTest < Minitest::Test
 
@@ -14,6 +14,28 @@ class FoodTruckTest < Minitest::Test
   end
 
   def test_it_has_stock
+    ft = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
+    assert_equal 0, ft.check_stock(item1)
+
+    ft.stock(item1, 30)
+    expect = {item1 => 30}
+
+    assert_equal expect, ft.inventory
+    assert_equal 30, ft.check_stock(item1)
+
+    ft.stock(item1, 25)
+    expect = {item1 => 55}
+
+    assert_equal expect, ft.inventory
+    assert_equal 55, ft.check_stock(item1)
+
+    ft.stock(item2, 12)
+    expect = {item1 => 55, item2 => 12}
+
+    assert_equal expect, ft.inventory
+    assert_equal 12, ft.check_stock(item2)
   end
 end
